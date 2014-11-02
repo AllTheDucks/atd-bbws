d6f5f3fcf307f397ba69212bbba6a43f9687c9e0
Wiley <wiley.fuller@gmail.com>
2014-11-02 22:16:56 +1100

    First version of Skeleton B2

---
+++ src/main/java/edu/myinst/stripes/HelloWorldAction.java
@@ -0,0 +1,18 @@
+package org.oscelot.jshack.stripes;
+
+import net.sourceforge.stripes.action.*;
+import net.sourceforge.stripes.integration.spring.SpringBean;
+
+
+public class HelloWorldAction implements ActionBean {
+
+    private ActionBeanContext context;
+    	
+	public ActionBeanContext getContext() {
+        return context;
+    }
+
+    public void setContext(ActionBeanContext context) {
+        this.context = context;
+    }
+}
